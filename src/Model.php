4c2e36091ba77679c60cbbc5f6d0cdf0be392224
Jack Jackdaw <j@ckdaw.net>
2026-07-09 14:54:29 +0300

    Use ?\DateTimeImmutable and added rawProperties method,.

--- src/Model.php
+++ src/Model.php
@@ -7,12 +7,12 @@ abstract class Model implements \IteratorAggregate, \Traversable, \JsonSerializa
     public function copy(array $udpatedData): Model
     {
         $class = get_class($this);
-        return new $class(...[...$this, ...$udpatedData]);
+        return new $class(...[...$this->rawProperties(), ...$udpatedData]);
     }
 
     public function getIterator(): \Traversable
     {
-        return new \ArrayIterator($this->toArray());
+        return new \ArrayIterator($this->rawProperties());
     }
 
     public static function __set_state(array $data)
@@ -28,30 +28,37 @@ abstract class Model implements \IteratorAggregate, \Traversable, \JsonSerializa
 
     public function toArray(): array
     {
-        return array_map(
-            fn($prop) => $prop->getValue($this),
-            (new \ReflectionClass($this))->getProperties()
-        );
+        return $this->rawProperties();
+    }
+
+    protected function rawProperties(): array
+    {
+        $properties = [];
+        foreach ((new \ReflectionClass($this))->getProperties() as $prop) {
+            $properties[$prop->getName()] = $prop->getValue($this);
+        }
+
+        return $properties;
     }
 
     protected function formatDate(mixed $date): ?string
     {
         $date = $this->buildDate($date);
-        if ($date instanceof \DateTime) {
+        if ($date instanceof \DateTimeImmutable) {
             return $date->format('c');
         }
 
         return $date;
     }
 
-    protected function buildDate(mixed $date): ?\DateTime
+    protected function buildDate(mixed $date): ?\DateTimeImmutable
     {
         if ($date === null) {
             return null;
         }
 
         if (is_string($date)) {
-            $date = new \DateTime($date);
+            $date = new \DateTimeImmutable($date);
         }
 
         return $date;
